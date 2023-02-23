02751bd5ca9a542d1d3394d37a65d6e7c606d770
Pranjal Pratap Singh <69805496+yyppsk@users.noreply.github.com>
2023-02-23 12:14:18 +0530

    Add files via upload

--- filehandling.c
+++ filehandling.c
@@ -1,7 +1,7 @@
 /*
 Your task is to code file handling code in your preferred language. Here if you have chosen C.
 
-Write a program in C for file handling, creation of a file and writing the provided string in it.4
+Write a program in C for file handling, creation of a file and writing the provided string in it.
 
 File has to be .txt
 
--- filehandling.py
+++ filehandling.py
@@ -1,7 +1,7 @@
 '''
 Your task is to code file handling code in your preferred language. Here you have chosen Python.
 
-Write a program in Python for file handling, creation of a file and writing the provided string in it.4
+Write a program in Python for file handling, creation of a file and writing the provided string in it.
 
 File has to be .txt
 '''
